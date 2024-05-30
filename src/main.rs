51a2b19a27d17981db5f13a24e6bfbe170fb7dc2
Nicolas Peralta Paez <c0932238@mylambton.ca>
2024-05-30 19:22:08 -0400

    Comfy first program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use comfy::*;
+
+simple_game!("Nice red circle", update);
+
+fn update(_c: &mut EngineContext) {
+   draw_circle(vec2(0.0, 0.0), 0.5, RED, 0);
 }
