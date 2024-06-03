8cdd4bd1a39f588a402951336431383fe355934c
Nicolas Peralta <nperaltapz@gmail.com>
2024-06-02 20:07:25 -0400

    referencing some variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("This is a ");
+    string_adder(&mut s);
+    println!("{}", s);
+
+    let reference = &s;
+    println!("inmutable reference: {}", reference); 
+
+}
+
+fn string_adder (s: &mut String) {
+    s.push_str("string");
 }
