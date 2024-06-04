ebee7028f49380c3fdedf28071dd79b543072859
Nicolas Peralta <nperaltapz@gmail.com>
2024-06-03 20:50:55 -0400

    first simulation

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,15 @@
-#[derive(Debug)]
-enum SoldierType {
-    Arquero,
-    Infante,
-    Caballero,    
+use rand::Rng
+
+struct Player{
+    position: (i32, i32),
+    live_player_close: bool,
+    player_detected: bool, 
 }
 
-#[derive(Debug)]
-struct Soldier {
-    soldier_type: SoldierType,
-}
-
-impl Soldier {
-    fn new (st: SoldierType) -> Soldier{
-        Soldier{soldier_type:st}
-    }
-
+impl Player {
+    fn move {}
 }
 
 fn main() {
-    // let mut s = String::from("This is a ");
-    // string_adder(&mut s);
-    // println!("{}", s);
 
-    // let reference = &s;
-    // println!("inmutable reference: {}", reference); 
-
-    let primer_recluta = Soldier::new(SoldierType::Arquero);
-    println!("{:?}, {:?}", primer_recluta, primer_recluta.soldier_type);
 }
-
-// fn string_adder (s: &mut String) {
-//     s.push_str("string");
-// }
