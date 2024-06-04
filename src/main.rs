ae3e95ef7570346a080f365b82da5932f19e1908
Nicolas Peralta <nperaltapz@gmail.com>
2024-06-03 20:59:20 -0400

    new structs for Game and Game_board

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 use rand::Rng
 
+// Game
+struct Game {
+    board: Board,
+    players: (Player, Player),
+    on_game: bool,
+}
+
+// Game board
+struct Board {
+    size: (i32, i32),
+}
+
+// Player
+
+enum PlayerMovements {
+    Quiet,
+    Attack,
+    Prowl,
+}
+
+
 struct Player{
+    name: String,
     position: (i32, i32),
     live_player_close: bool,
     player_detected: bool, 
+
 }
 
+
 impl Player {
-    fn move {}
+    fn movement(&mut self) {
+        
+    }
 }
 
 fn main() {
